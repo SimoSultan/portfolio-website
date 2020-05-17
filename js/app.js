0ab6548c2c17a7ca1a9ac6a8b61ac4213d301267
SimoSultan <simon.curran.89@gmail.com>
2020-05-18 00:46:05 +1000

    added animations to home page

--- js/app.js
+++ js/app.js
@@ -8,3 +8,63 @@ function pressMainButton() {
   }
   
 };
+
+// function test() {
+//   let tooltipHover;
+//   tooltipHover = "."+$(this).attr("class")+"-tip";
+//   // console.log(tooltipHover);
+//   // console.log(this);
+//   console.log( 'hovering on' , $(this).attr('id') );
+//   console.log(this.$.attr);
+//   // $(
+//   //   $(".nav-links .nav-text").hover(function() {
+//   //     tooltipHover = "."+$(this).attr("class")+"-tip";
+//   //     console.log(tooltipHover);
+      
+//   //     $(tooltipHover).addClass("active")
+//   //   }, function() {
+//   //     $(tooltipHover).removeClass("active");
+//   //   })
+//   // )
+
+
+// }
+
+// $('nav').hover(function() {
+//   // alert(this.class);
+//   console.log("hello");
+//   // console.log($(this));
+  
+// });
+// $('nav').mouseover(function() {
+//   console.log("hello");
+  
+//   console.log($(this));
+//   // function(){ $(this).addClass('hover') },
+//   // function(){ $(this).removeClass('hover') }
+// });
+
+
+// $("nav").on({
+//   mouseenter: function () {
+//     console.log("hello");
+    
+//     //  $(this).addClass("hover");
+//   },
+//   mouseleave:function () {
+//     console.log("bye");
+//     // $(this).removeClass("hover");
+//   }
+// });
+
+// $(".nav-links").hover(
+//   function () {
+//     console.log("hello");
+    
+//     // $(this).append($("<span> ***</span>"));
+//   }, 
+//   function () {
+//     console.log("bye");
+//     // $(this).find("span:last").remove();
+//   }
+// );
