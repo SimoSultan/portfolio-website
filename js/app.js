3440d14725829cea1ec7de12b82fd24145625a5d
SimoSultan <simon.curran.89@gmail.com>
2020-05-17 18:28:22 +1000

    commented out pixel ratio desktop media query

--- js/app.js
+++ js/app.js
@@ -1,10 +1,15 @@
 
-function pressMainButton() {
-  if ($("#hidden").is(":visible")) {
-    $("#hidden").hide()
+// function pressMainButton() {
+//   if ($("#hidden").is(":visible")) {
+//     $("#hidden").hide()
 
-  } else {
-    $("#hidden").show()
-  }
+//   } else {
+//     $("#hidden").show()
+//   }
   
-};
+// };
+
+
+function linkToPage(page) {
+  window.location.href = page;
+}
