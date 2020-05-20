3d62547f414978d674bef188a1a99a0942146ae8
SimoSultan <simon.curran.89@gmail.com>
2020-05-20 13:28:38 +1000

    started about page

--- js/app.js
+++ js/app.js
@@ -1,81 +1,19 @@
 
-// function pressMainButton() {
-//   if ($("#hidden").is(":visible")) {
-//     $("#hidden").hide()
-
-//   } else {
-//     $("#hidden").show()
-//   }
-  
+// function linkToPage(page) {
+//   window.location.href = page;
 // };
-
-
-function linkToPage(page) {
-  window.location.href = page;
-};
-
-// function test() {
-//   let tooltipHover;
-//   tooltipHover = "."+$(this).attr("class")+"-tip";
-//   // console.log(tooltipHover);
-//   // console.log(this);
-//   console.log( 'hovering on' , $(this).attr('id') );
-//   console.log(this.$.attr);
-//   // $(
-//   //   $(".nav-links .nav-text").hover(function() {
-//   //     tooltipHover = "."+$(this).attr("class")+"-tip";
-//   //     console.log(tooltipHover);
-      
-//   //     $(tooltipHover).addClass("active")
-//   //   }, function() {
-//   //     $(tooltipHover).removeClass("active");
-//   //   })
-//   // )
-
-
-// }
-
-// $('nav').hover(function() {
-//   // alert(this.class);
-//   console.log("hello");
-//   // console.log($(this));
-  
-// });
-// $('nav').mouseover(function() {
-//   console.log("hello");
-  
-//   console.log($(this));
-//   // function(){ $(this).addClass('hover') },
-//   // function(){ $(this).removeClass('hover') }
-// });
-
-
-// $("nav").on({
-//   mouseenter: function () {
-//     console.log("hello");
-    
-//     //  $(this).addClass("hover");
-//   },
-//   mouseleave:function () {
-//     console.log("bye");
-//     // $(this).removeClass("hover");
-//   }
-// });
-
-// $(".nav-links").hover(
-//   function () {
-//     console.log("hello");
-    
-//     // $(this).append($("<span> ***</span>"));
-//   }, 
-//   function () {
-//     console.log("bye");
-//     // $(this).find("span:last").remove();
-//   }
-// );
 
 
 $(window).scroll(function(){
   $(".arrow").css("opacity", 1 - $(window).scrollTop() / 250); 
 //250 is fade pixels
+});
+
+const btnScrollToAbout = document.querySelector("#scroll-to-about");
+btnScrollToAbout.addEventListener("click", function() {
+  // $("html, body").animate({scrollTop: 0, "slow"});
+  $("#about-container").show();
+  // $("#home-container").hide();
+
+  $(window).scrollTop($('#about-container').offset().top);
 });
